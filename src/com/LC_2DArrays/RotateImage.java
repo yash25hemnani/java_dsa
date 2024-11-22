8272e1adef299f623f08207608deda2900c0be91
Yash <yashhemnanigit@gmail.com>
2024-11-22 20:54:30 +0530

    New Question

---
+++ src/com/LC_2DArrays/RotateImage.java
@@ -0,0 +1,61 @@
+package com.LC_2DArrays;
+
+import java.util.Arrays;
+
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        transpose(arr);
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        reverseRow(arr);
+        for(int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    static void transpose(int[][] matrix){
+        // Iterate over only the upper triangle of the matrix --> elements will be swapped
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    static void reverseRow(int[][] matrix){
+        for(int i = 0; i < matrix.length; i++){
+            int start = 0;
+            int end = matrix[0].length - 1;
+
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
